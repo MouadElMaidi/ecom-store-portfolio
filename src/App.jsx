527f307075e72c5d4168fde0b4c8181ff905c8ef
MouadElMaidi <mouadlmaidi@gmail.com>
2023-10-20 14:09:02 +0100

    Added Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,47 @@
-function App() {
-  return <h1>Hello World!</h1>;
-}
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import {
+  RootLayout,
+  HomePage,
+  AboutPage,
+  ProductsPage,
+  SingleProductPage,
+  ErrorPage,
+  CartPage,
+} from "./pages";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: "/about",
+        element: <AboutPage />,
+      },
+      {
+        path: "/products",
+        element: <ProductsPage />,
+      },
+      {
+        path: "/cart",
+        element: <CartPage />,
+      },
+      {
+        path: "/:productId",
+        element: <SingleProductPage />,
+      },
+    ],
+  },
+]);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
 
 export default App;
---
+++ src/pages/index.jsx
@@ -0,0 +1,17 @@
+import HomePage from "./Home";
+import RootLayout from "./Root";
+import ProductsPage from "./Products";
+import AboutPage from "./About";
+import SingleProductPage from "./SingleProduct";
+import ErrorPage from "./Error";
+import CartPage from "./Cart";
+
+export {
+  HomePage,
+  RootLayout,
+  ProductsPage,
+  AboutPage,
+  SingleProductPage,
+  ErrorPage,
+  CartPage,
+};
